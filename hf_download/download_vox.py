c6d14c138b5a14f52d78908b6ed6406114f54775
Lin Geyu <geyu.lin@u.nus.edu>
2023-09-13 19:11:35 +0800

    update script

--- hf_download/download_vox.py
+++ hf_download/download_vox.py
@@ -1,17 +1,19 @@
 from datasets import load_dataset
 import fire
+import os
 
-# langs = ['en', 'de', 'fr', 'es', 'pl', 'it', 'ro', 
- # 'hu', 'cs', 'nl', 'fi', 'hr', 'sk', 'sl', 'et', 'lt', 'en_accented']
+langs = ['et', 'en', 'de', 'fr', 'es', 'pl', 'it', 'ro', 
+ 'hu', 'cs', 'nl', 'fi', 'hr', 'sk', 'sl',  'lt', 'en_accented']
 
-def download_ds(lang: str, dest: str, num_worker:int = 4):
-    print('Downloading and load dataset from hugging face hub...')
-    ds = load_dataset('facebook/voxpopuli', lang, num_proc=num_worker)
-    print('Dataset loaded')
-
-    print('Saving dataset...')
-    ds.save_to_disk(dest, num_proc=num_worker)
-    print('Dataset saved.')
+def download_ds(dest: str, num_worker:int = 4):
+    for lang in langs:
+        print(f'Downloading and load {lang} dataset from hugging face hub...')
+        ds = load_dataset('facebook/voxpopuli', lang, num_proc=num_worker)
+        print('Dataset loaded')
+        dest = os.path.join(dest, lang)
+        print(f'Saving dataset to {dest}')
+        ds.save_to_disk(dest, num_proc=num_worker)
+        print('Dataset saved.')
 
 if __name__ == "__main__":
     fire.Fire(download_ds)
