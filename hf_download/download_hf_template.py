c65476cec17939015c4d79e0e1104f979d89a629
LGY <geyu.lin@u.nus.edu>
2023-09-14 18:53:05 +0800

    set cache folder

--- hf_download/download_hf_template.py
+++ hf_download/download_hf_template.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import fire
 def download_ds(ds_hf_name:str, dest: str, num_worker:int = 4):
     print('Downloading and load dataset from hugging face hub...')
-    ds = load_dataset(ds_hf_name, num_proc=num_worker)
+    ds = load_dataset(ds_hf_name, num_proc=num_worker, cache_dir=dest)
     print('Dataset loaded')
 
     print('Saving dataset...')
