50b8a2910070c82e4bc3ac12500df65bc0bb7695
Lin Geyu <geyu.lin@u.nus.edu>
2023-10-03 11:02:11 +0800

    add script for people speech

---
+++ hf_download/download_peoplespeech.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+import fire
+
+import os
+
+def download_ds(dest: str, num_worker:int = 4):
+    print('Downloading and load dataset from hugging face hub...')
+    
+    ds = load_dataset('MLCommons/peoples_speech', num_proc=num_worker)
+    print('Dataset loaded')
+
+    print('Saving dataset...')
+    ds.save_to_disk(dest, num_proc=num_worker)
+    print('Dataset saved.')
+
+    ds.cleanup_cache_files()
+    print('Local cache cleared')
+
+if __name__ == "__main__":
+    fire.Fire(download_ds)
