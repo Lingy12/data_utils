f46f9d1e3f15d06751843731a8b94398e5de4ce9
Lin Geyu <geyu.lin@u.nus.edu>
2023-10-18 15:13:15 +0800

    add hugging face for malay corpus

---
+++ in_house_ds/malay_corpora_cty.py
@@ -0,0 +1,147 @@
+import fire
+import os
+from glob import glob
+from pydub import AudioSegment
+import shutil
+import multiprocessing as mp
+from tqdm import tqdm
+from datasets import Dataset, Audio
+
+# function to segment a speakers audio file
+def segment(args): # speaker level output folder
+    wav_file, seg_lst, output_folder = args
+    failed_lst = []
+    audio = AudioSegment.from_wav(wav_file)
+        
+    for seg in seg_lst:
+        id, start, end, script = seg
+        curr_seg = audio[int(start) * 10:int(end) * 10]
+        seg_name = '-'.join([id, start, end])
+        trans_path, wav_path = os.path.join(output_folder, seg_name + '.txt'), os.path.join(output_folder, seg_name + '.wav')
+        if len(curr_seg) == 0:
+            failed_lst.append(seg_name)
+        else:
+            with open(trans_path, 'w') as f:
+                f.write(script)
+            curr_seg.export(wav_path, format='wav')
+    return failed_lst
+
+def generate_dict_for_speaker(args):
+    directory = args
+    speaker_dict = {"audio":[], "utt_id":[], "speaker":[], "sentence": []}
+
+    audios = glob(os.path.join(directory, '*.wav')) 
+    # print(directory, audios)
+    for audio in audios:
+        script_file = audio[:-4] + '.txt'
+        speaker = audio.split('/')[-2]
+        utt_id = audio[:-4]
+        with open(script_file, 'r') as f:
+            sentence = f.read()
+        for attr in speaker_dict.keys():
+            speaker_dict[attr].append(eval(attr))
+    return speaker_dict
+class MalayCorp:
+    def __init__(self, folder, info_folder):
+        self.folder_map = {
+                "king": os.path.join(folder, "malay_king223"),
+                "ntubn": os.path.join(folder, "malay_ntubn"),
+                "ntucs": os.path.join("malay_ntucs")
+                }
+        self.folder = folder
+        self.info_folder = info_folder
+    
+    def parse_file_to_map(self, file):
+        res = {}
+        with open(file, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            key, value = line.split(' ', 1)
+            res[key] = value
+        return res
+
+    def ds_check(self):
+        for split in self.folder_map.keys():
+            print('Checking split: ' + split)
+
+            audio_files = glob(os.path.join(self.folder, self.folder_map[split], 'wav', '*.wav'))
+            wav_map = self.parse_file_to_map(os.path.join(self.info_folder, split, 'wav.scp'))
+
+            print('No. of audio = {}'.format(len(audio_files)))
+            print('No. of audio in record = {}'.format(len(wav_map)))
+                
+    def group_by_speaker(self, transcripts):
+        res = {}
+        # print(transcripts)
+        for id in transcripts.keys():
+            script = transcripts[id]
+            speaker, start, end = id.split('-')
+            if speaker not in res:
+                res[speaker] = []
+            res[speaker].append((speaker, start, end, script))
+        return res
+
+    def segment_audios(self, output_folder, num_worker=4):
+        # found 1376MM1
+        if os.path.exists(output_folder):
+            need_rm = input("The folder exists, do you want to remove? (y to remove)")
+            if need_rm:
+                shutil.rmtree(output_folder)
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        failed_lst = []
+        # for each participant, create folder and check list
+        for split in self.folder_map.keys():
+            split_dir = os.path.join(output_folder, split)
+            if not os.path.exists(split_dir):
+                os.mkdir(split_dir)
+
+            transcripts = self.parse_file_to_map(os.path.join(self.info_folder, split, 'text')) # parse transcript
+
+            speaker_seg_map = self.group_by_speaker(transcripts)
+            params = []
+            for speaker in speaker_seg_map.keys():
+                speaker_folder = os.path.join(split_dir, speaker)
+                speaker_wav = os.path.join(self.folder, self.folder_map[split], 'wav', speaker + '.wav')
+                if not os.path.exists(speaker_folder):
+                    os.mkdir(speaker_folder)
+                params.append((speaker_wav, speaker_seg_map[speaker], speaker_folder))
+            print('Starting jobs for split: ' + split)
+            with mp.Pool(num_worker) as p:
+                results = list(tqdm(p.imap(segment, params), total = len(params)))
+            for res in results:
+                failed_lst += res
+            
+        print('Failed list: ')
+        print(failed_lst)
+   
+    def build_hf_ds(self, segmented_folder, output_folder, num_workers=4):
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        for split in self.folder_map.keys():
+            split_dict = {"audio":[], "utt_id":[], "speaker":[], "sentence": []}
+            target_hf_folder = os.path.join(output_folder, split)
+            
+            if not os.path.exists(target_hf_folder):
+                os.mkdir(target_hf_folder)
+            params = []
+
+            for directory in os.listdir(os.path.join(segmented_folder, split)):
+                params.append(os.path.join(segmented_folder, split, directory))
+
+            with mp.Pool(num_workers) as p:
+                r = list(tqdm(p.imap(generate_dict_for_speaker, params), total=len(params)))
+            
+            for speaker_dict in r:
+                for key in split_dict.keys():
+                    split_dict[key] += speaker_dict[key]
+
+            # print(split_dict)
+            split_ds = Dataset.from_dict(split_dict)
+            split_ds = split_ds.cast_column('audio', Audio())
+            split_ds.save_to_disk(target_hf_folder, num_proc=num_workers)
+            print(split + ' split saved')
+                
+
+if __name__ == "__main__":
+    fire.Fire(MalayCorp)
