59256306882524ec15d01ec8438c6ec3d68c63fa
Lin Geyu <geyu.lin@u.nus.edu>
2023-10-22 17:44:02 +0800

    change shard strategy

--- create_hf/common_voice/build_common_voice_ds.py
+++ create_hf/common_voice/build_common_voice_ds.py
@@ -17,6 +17,7 @@ def build_language_ds_pd(args):
     clips_directory = os.path.join(target_folder, 'clips')
     dfs = []
     status = 1
+    count = 0
     # print(target_folder)
     for split in splits:
         # print(split)
@@ -35,8 +36,9 @@ def build_language_ds_pd(args):
         split_data['split'] = split[:-4] # append split
         split_data['audio'] = split_data['path'].map(lambda x: os.path.join(clips_directory, x))
         split_data['language'] = language    
+        count += len(split_data)
         dfs.append(split_data.copy())
-    return Dataset.from_pandas(pd.concat(dfs), preserve_index=False).cast_column('audio', Audio()), language, status
+    return Dataset.from_pandas(pd.concat(dfs), preserve_index=False).cast_column('audio', Audio()), language, status, int(count / 10000) + 1
 
 def build_hf_ds(raw_data_dir, dest, num_workers = 4):
     language_dir = os.listdir(raw_data_dir)
@@ -53,17 +55,19 @@ def build_hf_ds(raw_data_dir, dest, num_workers = 4):
         r = list(tqdm(p.imap(build_language_ds_pd, params), total=len(params)))
     
     ds_dict = DatasetDict()
+    num_shards = {}
     for res in r:
-        ds, language, status = res
+        ds, language, status, n_shard = res
         if status == 0:
             print('Please check tsv first.')
             exit()
         ds_dict[language] = ds
+        num_shards[language] = n_shard
         print(language + ' ds added.')
         print('Total rows = {}'.format(len(ds)))
 
     print(ds_dict)
-    ds_dict.save_to_disk(dest, num_proc=num_workers)
+    ds_dict.save_to_disk(dest, num_proc=num_workers, num_shards=num_shards)
 
 if __name__ == "__main__":
     fire.Fire(build_hf_ds)
