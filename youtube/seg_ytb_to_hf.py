5999a5e39177d3849608f9636866110d4fbc0675
Lin Geyu <geyu.lin@u.nus.edu>
2024-04-18 18:31:25 +0800

    update script to segment youtube audio

--- youtube/seg_ytb_to_hf.py
+++ youtube/seg_ytb_to_hf.py
@@ -1,14 +1,17 @@
 import fire
-from datasets import load_from_disk, Dataset, Audio, Features
+from datasets import load_from_disk, Dataset, Audio, Features, Value
 from dataclasses import dataclass
 from typing import List
 import math
 import os
+import numpy as np
 
-def split_audio(raw_array, sampling_rate):
+def split_audio(raw_audio, sampling_rate):
+    raw_array = raw_audio['array']
     total_segment = math.ceil(len(raw_array) / (sampling_rate * 30.0))
-    step = sampling_rate * 30.0
+    step = sampling_rate * 30
     n = len(raw_array)
+    # print(raw_array)
     return [{"audio": raw_array[i * step: min((i + 1) * step, n)], "sampling_rate": sampling_rate} for i in range(total_segment)]
 
 # split for unsupervised dataset
@@ -17,10 +20,10 @@ class SegmenteAudio(object):
     sampling_rate: int 
 
     def __call__(self, batch):
-        output_batch = {k : [] for k in batch.keys() + "segment_id"}
+        output_batch = {k : [] for k in set(batch.keys()).union(set(["segment_id"]))}
         
         audios = batch['audio']
-        keep_keys = output_batch.keys() - set(['audio', "segment_id"]) 
+        keep_keys = set(output_batch.keys()) - set(['audio', "segment_id"]) 
         
         for i in range(len(audios)):
             audio = audios[i]
@@ -50,20 +53,25 @@ def segment_ytb_to_hf(raw_folder, output_path, batch_size=10, workers=4):
                 ytb_id = os.path.splitext(file_name)[0]
                 
                 # Append the data to the dictionary
-                ori_dict["audio"].append(file_name)
+                ori_dict["audio"].append(file_path)
                 ori_dict["parent"].append(base_dir)
                 ori_dict["sentence"].append("")  # Assuming 'sentence' to be filled later or elsewhere
                 ori_dict["ytb_id"].append(ytb_id)
     
-    
+    print(ori_dict)
     ori_ds = Dataset.from_dict(ori_dict)
     ori_ds = ori_ds.cast_column('audio', Audio(sampling_rate=16000))
     split_funct = SegmenteAudio(sampling_rate=16000)
     
-    features_dict = {k: ori_ds.features[k] for k in ori_ds}
+    # print(ori_ds)
+    features_dict = {k: ori_ds.features[k] for k in ori_ds.column_names}
+    features_dict.update({"segment_id": Value(dtype='int8')})
+    print(features_dict)
     features = Features(features_dict)
-    
     splited_ds = ori_ds.map(split_funct, batched=True, num_proc=workers, batch_size=batch_size, 
                 remove_columns=ori_ds.column_names, features=features)
     
     splited_ds.save_to_disk(output_path)
+
+if __name__ == "__main__":
+    fire.Fire(segment_ytb_to_hf)
