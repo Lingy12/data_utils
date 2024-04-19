ace94c6be4eca83f08d3532328201400b11ef068
Lin Geyu <geyu.lin@u.nus.edu>
2024-04-19 16:29:58 +0800

    add benchmark conversion

---
+++ benchmark/build_hf_for_all.sh
@@ -0,0 +1,10 @@
+python convert_to_hf.py EN_Benchmark_CNA_set bench_hf/cna_hf 
+
+python convert_to_hf.py EN_Benchmark_IDPC bench_hf/idpc_hf 1
+
+python convert_to_hf.py EN_Benchmark_MediaCorp bench_hf/mediacorp_hf
+
+python convert_to_hf.py EN_Benchmark_Parliament_set bench_hf/parliament_hf 1
+
+python convert_to_hf.py EN_Benchmark_UKUS_news_set bench_hf/ukusnews 1
+
---
+++ benchmark/convert_to_hf.py
@@ -0,0 +1,52 @@
+import os
+import fire
+from datasets import Dataset, Audio
+
+def find_audio_files(root_dir):
+    """Find all .wav files within the 'audio' directories under the given root directory."""
+    audio_files = []
+    for subdir, dirs, files in os.walk(root_dir):
+        if 'audio' in os.path.basename(subdir):
+            for file in files:
+                if file.endswith('.wav'):
+                    audio_files.append(os.path.join(subdir, file))
+    return audio_files
+
+def load_transcripts(text_file):
+    """Load transcripts from a text file."""
+    transcript_map = {}
+    with open(text_file, 'r') as file:
+        for line in file:
+            parts = line.strip().split(' ', 1)
+            if len(parts) > 1:
+                transcript_map[parts[0]] = parts[1]
+    return transcript_map
+
+def create_dataset(audio_files, transcript_map):
+    """Create a dictionary with lists for files and transcripts."""
+    data = {'audio': [], 'sentence': [], 'file_name': []}
+    for audio_file in audio_files:
+        filename = os.path.basename(audio_file).split('.')[0]
+        transcript = transcript_map.get(filename, '')
+        data['audio'].append(audio_file)
+        data['file_name'].append(audio_file)
+        data['sentence'].append(transcript)
+    return Dataset.from_dict(data)
+
+def build_data(root_dir, output_path, num_workers=16):
+# Set your root directory here
+    os.makedirs(output_path, exist_ok=True)
+    text_file = os.path.join(root_dir, 'text')
+
+    # Process files
+    audio_files = find_audio_files(root_dir)
+    transcript_map = load_transcripts(text_file)
+    dataset = create_dataset(audio_files, transcript_map)
+    dataset = dataset.cast_column('audio', Audio(sampling_rate=16000))
+
+    print(dataset)
+    dataset.save_to_disk(output_path, num_proc=num_workers)
+
+if __name__ == "__main__":
+    fire.Fire(build_data)
+
