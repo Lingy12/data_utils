a91795999ae104f3ca22a0aa76f4715872c5ef3e
Lin Geyu <geyu.lin@u.nus.edu>
2024-04-19 23:50:40 +0800

    fix benchmark logic

--- benchmark/convert_to_hf.py
+++ benchmark/convert_to_hf.py
@@ -4,12 +4,14 @@ from datasets import Dataset, Audio
 
 def find_audio_files(root_dir):
     """Find all .wav files within the 'audio' directories under the given root directory."""
-    audio_files = []
+    audio_files = {}
     for subdir, dirs, files in os.walk(root_dir):
         if 'audio' in os.path.basename(subdir):
             for file in files:
                 if file.endswith('.wav'):
-                    audio_files.append(os.path.join(subdir, file))
+                    file_path = os.path.join(subdir, file)
+                    filename = os.path.basename(file_path).split('.')[0]
+                    audio_files[filename] = file_path
     return audio_files
 
 def load_transcripts(text_file):
@@ -22,26 +24,27 @@ def load_transcripts(text_file):
                 transcript_map[parts[0]] = parts[1]
     return transcript_map
 
-def create_dataset(audio_files, transcript_map):
+def create_dataset(transcript_map, audio_files):
     """Create a dictionary with lists for files and transcripts."""
     data = {'audio': [], 'sentence': [], 'file_name': []}
-    for audio_file in audio_files:
-        filename = os.path.basename(audio_file).split('.')[0]
-        transcript = transcript_map.get(filename, '')
-        data['audio'].append(audio_file)
-        data['file_name'].append(audio_file)
-        data['sentence'].append(transcript)
+    for filename, transcript in transcript_map.items():
+        audio_file = audio_files.get(filename)
+        if audio_file:
+            data['audio'].append(audio_file)
+            data['file_name'].append(audio_file)
+            data['sentence'].append(transcript)
+        else:
+            print(f"Warning: Audio file for {filename} not found.")
     return Dataset.from_dict(data)
 
 def build_data(root_dir, output_path, num_workers=16):
-# Set your root directory here
     os.makedirs(output_path, exist_ok=True)
     text_file = os.path.join(root_dir, 'text')
 
     # Process files
     audio_files = find_audio_files(root_dir)
     transcript_map = load_transcripts(text_file)
-    dataset = create_dataset(audio_files, transcript_map)
+    dataset = create_dataset(transcript_map, audio_files)
     dataset = dataset.cast_column('audio', Audio(sampling_rate=16000))
 
     print(dataset)
