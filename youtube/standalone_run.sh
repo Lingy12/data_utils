5d1ddb7a43eb6f9590eec411bc4981d6f94e6f1c
Lin Geyu <lingy@asp2a-login-astar01.head.cm.asp2a.nscc.sg>
2024-04-21 19:21:04 +0800

    add standalone run

---
+++ youtube/standalone_run.sh
@@ -0,0 +1,7 @@
+channel=$1
+output_path=$2
+num_of_node=$3
+local_rank=$4
+
+python download_channels.py fetch_video_metadata $channel $output_path
+python download_channels.py worker_process $output_path --workers 16 --local_rank $local_rank --num_ranks $num_of_node --max_files_in_folder 100
