8979096abcc4585812e9a420f3159532f4b8ad43
Lin Geyu <you@example.com>
2024-04-23 06:22:10 +0300

    fix typo

--- youtube/lumi/scrapt_youtube_data.sh
+++ youtube/lumi/scrapt_youtube_data.sh
@@ -8,6 +8,7 @@ num_of_node=$3  # At most 15
 partition=$4
 workers_per_node=$5
 
+module load LUMI/22.06  partition/C
 # Ensure num_of_node does not exceed 15
 if [ "$num_of_node" -gt 15 ]; then
   echo "num_of_node cannot be greater than 15"
@@ -38,7 +39,7 @@ if [ ! -f "$output_path/metadata.json" ]; then
         sbatch --job-name=$name.$i --dependency=afterok:$metadata_job_id \
                     --output=$output_path/log/$name.$i.log \
                         --partition=$partition --nodes=$num_of_node \ 
-                        --ntasks-per-node=$workers_per_node --cpu-per-task=1 \ 
+                        --ntasks-per-node=$workers_per_node --cpus-per-task=1 --gpus-per-node=0 \ 
                         --mem=480G bash lumi/download_wrapper.sh \
                         $channel $output_path $num_of_node $workers_per_node
         # qsub -W depend=afterok:$metadata_job_id -v channel="$channel",output_path="$output_path",local_rank="$i",num_of_node="$num_of_node",name="$name" \
@@ -53,7 +54,7 @@ else
         sbatch --job-name=$name.$i \
             --output=$output_path/log/$name.$i.log \
                 --partition=$partition --nodes=$num_of_node \ 
-                --ntasks-per-node=$workers_per_node --cpu-per-task=1 \ 
+                --ntasks-per-node=$workers_per_node --cpus-per-task=1 --gpus-per-node=0\ 
                 --mem=480G bash lumi/download_wrapper.sh \
                 $channel $output_path $num_of_node $workers_per_node
     done
