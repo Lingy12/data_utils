7df094d22f7db781bc865290b254ae54a6edf257
Lin Geyu <geyu.lin@u.nus.edu>
2024-04-25 14:52:21 +0800

    fix script

--- create_hf/imda/build_hf_part3.py
+++ create_hf/imda/build_hf_part3.py
@@ -1,4 +1,5 @@
-from datasets import Audio, load_from_disk
+from datasets import Audio, load_from_disk, Dataset
+from tqdm import tqdm
 from datasets.features import Value, Sequence
 import fire
 import multiprocessing
@@ -15,8 +16,8 @@ SCRIPT_PREFIX = 'Scripts'
 def fetch_metadata(root):
     
     metadata = os.path.join(root, 'Documents', "Speakers_(Part_3).XLSX")
-    records = pd.read_excel(metadata).to_dict(orient='records')
-
+    records = pd.read_excel(metadata).fillna('Unknown').to_dict(orient='records')
+    
     # build speaker dictionary
     print('Record before build = {}'.format(len(records)))
     record_dict = {d['SCD']: d for d in records}
@@ -47,92 +48,75 @@ def get_transcriptions(root, script_type, mictype, wav_file_path):
             return None, conversation_id
 
 def map_intervals(intervals, path):
-    return {"chunks": [{"start": interval.minTime, "end": interval.maxTime, "sentence": interval.mark} for interval in intervals],
+    # print(intervals)
+    return {"chunks": [{"start": interval.minTime, "end": interval.maxTime, "sentence": interval.mark} for interval in intervals[0]],
             "path": path}
 
-def build_hf(root, workers=4):
+def process_file(args):
+    f, root, script_t, mic_t, speaker_metadata_dict = args
+    transcription_file, conversation_id = get_transcriptions(root, script_t, mic_t, f)
+    data = {
+        'audio': f,
+        'audio_path': f,
+        'metadata': {
+            'script_type': script_t,
+            'mic_type': mic_t,
+            'conversation_id': conversation_id
+        }
+    }
+    if transcription_file:
+        speaker_1, speaker_2 = transcription_file['speaker_1'], transcription_file['speaker_2']
+        try:
+            intervals_1 = map_intervals(textgrid.TextGrid.fromFile(speaker_1), speaker_1)
+            data['metadata']["chunk_speaker_1"] = intervals_1
+        except Exception as e:
+            print(e)
+            data['metadata']['chunk_speaker_1'] = None
 
+        try:
+            intervals_2 = map_intervals(textgrid.TextGrid.fromFile(speaker_2), speaker_2)
+            data['metadata']["chunk_speaker_2"] = intervals_2
+        except Exception as e:
+            print(e)
+            data['metadata']['chunk_speaker_2'] = None
+
+        if script_t == 'Same':
+            speaker1_id, speaker2_id = str(Path(os.path.basename(speaker_1)).with_suffix('')), str(Path(os.path.basename(speaker_2)).with_suffix(''))
+            print(speaker_metadata_dict.get(speaker1_id, ''))
+            data['metadata']['speaker_metadata'] = {
+                "speaker_1": speaker_metadata_dict.get(speaker1_id, ''),
+                "speaker_2": speaker_metadata_dict.get(speaker2_id, '')
+            }
+        else:
+            data['metadata']['speaker_metadata'] = None
+    return data
+
+def build_hf(root, output_path, workers=4):
     speaker_metadata_dict = fetch_metadata(root)
-    # print(metadata_dict)
-    
-    # mapping for script and mic type 
-    # scripts = {"Same": glob(os.path.join(root, "Scripts_Same", "*.TextGrid")), "Separate": glob(os.path.join(root, 'Scripts_Separate', "*.TextGrid"))}
     mic_type = {"Same": ["BoundaryMic", "CloseMic"], "Separate": ["IVR", 'StandingMic']}
-    failed_lst = []
-    
- #    schema = {
- #            "audio": Audio(sampling_rate=16000),
- #            "audio_path": Value(dtype='string'),
- #            "metadata": {
- #                'conversation_id': Value(dtype='int8'),
- #                "script_type": Value(dtype='string'),
- #                'mic_type': Value(dtype='string'),
- #                'chunk_speaker_1': {"chunks": Sequence(feature={'start': Value(dtype='float32'), 'end': Value(dtype='float32'), 
- #                                                                'sentence': Value(dtype='string')}), "path": Value(dtype='string')},
- #                'chunk_speaker_2': {"chunks":Sequence(feature={'start': Value(dtype='float32'), 'end': Value(dtype='float32'), 
- #                                                               'sentence': Value(dtype='string')}), "path": Value(dtype='string')}
- # 
- #                }
- #            }
-    # ds_dict = {k: [] for k in schema}
-    ds_dict = {"audio": [], "audio_path":[], "metadata": []}
+    ds_dict = {"audio": [], "audio_path": [], "metadata": []}
     
     for script_t in mic_type:
         for mic_t in mic_type[script_t]:
             relative_folder = '_'.join([AUDIO_PREFIX, script_t, mic_t])
             audio_folder = os.path.join(root, relative_folder)
             wav_files = glob(os.path.join(audio_folder, '**', '*.wav'), recursive=True)
-            for f in wav_files:
-                transcription_file, conversation_id = get_transcriptions(root, script_t, mic_t, f)
-                
-                ds_dict['audio'].append(f)
-                ds_dict['audio_path'].append(f)
 
-                metadata = {}
-                metadata['script_type'] = script_t
-                metadata['mic_type'] = mic_t
-                metadata['conversation_id'] = conversation_id
-                if transcription_file:
-                    speaker_1, speaker_2 = transcription_file['speaker_1'], transcription_file['speaker_2']
-                    try:
-                        intervals_1 = map_intervals(textgrid.TextGrid.fromFile(speaker_1), speaker_1)
-                        metadata["chunk_speaker_1"] = intervals_1
-                    except:
-                        metadata['chunk_speaker_1'] = None
+            pool = multiprocessing.Pool(processes=workers)
+            tasks = [(f, root, script_t, mic_t, speaker_metadata_dict) for f in wav_files]
+            
+            for result in tqdm(pool.imap(process_file, tasks), total=len(tasks), desc="Processing Files"):
+                ds_dict['audio'].append(result['audio'])
+                ds_dict['audio_path'].append(result['audio_path'])
+                ds_dict['metadata'].append(result['metadata'])
+            
+            pool.close()
+            pool.join()
 
-                    try:
-                        intervals_2 = map_intervals(textgrid.TextGrid.fromFile(speaker_2), speaker_2)
-                        metadata["chunk_speaker_2"] = intervals_2
-                    except:
-                        metadata['chunk_speaker_2'] = None
-                
-                    if script_t == 'Same':
-                        speaker1_id, speaker2_id = str(Path(os.path.basename(speaker_1)).with_suffix('')), str(Path(os.path.basename(speaker_2)).with_suffix(''))
-                        metadata['speaker_metadata'] = {"speaker_1": speaker_metadata_dict.get(speaker1_id, None), 
-                                                        "speaker_2": speaker_metadata_dict.get(speaker2_id, None)}
-                ds_dict['metadata'].append(metadata)
-    print(ds_dict)
-                
-    # for script_type in scripts:
-    #     type_script = scripts[script_type]
-    #
-    #     for s in type_script:
-    #         try:
-    #             intervals = textgrid.TextGrid.fromFile(s)[0]
-    #         except:
-    #             failed_lst.append(s)
-    #             print('Skipping {} due to the file limit'.format(s))
-    #             continue
-    #         
-    #         
-    #
-    #         for interval in intervals:
-    #             print(interval.minTime, interval.maxTime, interval.mark)
-    #             
-    #
-    #         print(intervals)
-    #         exit()
-    #
+    ds = Dataset.from_dict(ds_dict)
+    print(ds)
+    ds.save_to_disk(output_path, num_proc=workers)
+    # print(ds_dict)
 
     
 
