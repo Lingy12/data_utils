65b67f1d57563ac45782b39ef4086a1560bed919
Lin Geyu <geyu.lin@u.nus.edu>
2024-04-26 17:34:15 +0800

    update verify

--- post_process/youtube/verify_singlish.py
+++ post_process/youtube/verify_singlish.py
@@ -11,6 +11,8 @@ import fire
 def check_folder(root, trail):
     mp3_files = glob(os.path.join(root, '**', '*.mp3'), recursive=True)
     # print(mp3_files)
+    if len(mp3_files) < trail:
+        return {"ms": 0, "en": 0}
     def update_score(existing, added):
         for k in added:
             if added[k] < 0.1:
@@ -31,7 +33,9 @@ def check_folder(root, trail):
 def verify_folder(args):
     root, folder = args
     score = check_folder(os.path.join(root, folder), 50)
-        
+    print(folder, score)      
+    if 'ms' not in score:
+        return 0, folder
     if score['ms'] > 0.5 and score['en'] > 0.1:
         return 1, folder
     else:
@@ -39,9 +43,11 @@ def verify_folder(args):
 def verify_all(root, workers=4):
     sub_dirs = os.listdir(root)
     params = [(root, sub_dir) for sub_dir in sub_dirs]
-    with mp.Pool(processes=workers) as p:
-        results = tqdm(p.imap_unordered(verify_folder, params), total=len(params))
-    
+    # with mp.Pool(processes=workers) as p:
+    #     results = tqdm(p.imap_unordered(verify_folder, params), total=len(params))
+    results = []
+    for p in tqdm(params):
+        results.append(verify_folder(p))
     valid_channel = []
     for res in results:
         if res[0] == 1:
