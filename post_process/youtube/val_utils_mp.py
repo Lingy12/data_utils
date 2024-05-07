feca6ada1a6a8458e8bd1f9abbf78922154f72b1
Lin Geyu <geyu.lin@u.nus.edu>
2024-05-07 13:55:47 +0800

    support multiprocessing check

---
+++ post_process/youtube/val_utils_mp.py
@@ -0,0 +1,85 @@
+import torch
+import whisper
+import multiprocessing
+
+# torch.set_num_threads(1)
+
+# from IPython.display import Audio
+# from pprint import pprint
+import librosa
+
+USE_ONNX = False 
+
+vad_models = dict()
+whisper_models = dict()
+vad_utils = dict()
+
+def init_vad_model():
+    pid = multiprocessing.current_process().pid
+    model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                                model='silero_vad',
+                                force_reload=False,
+                                onnx=False)
+    vad_models[pid] = model.to('cuda')
+    vad_utils[pid] = utils
+    
+# def init_whisper_model():
+#     pid = multiprocessing.current_process().pid
+#     whisper_model = whisper.load_model("base")
+#     whisper_models[pid] = whisper_model
+
+# ## loading model
+# model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+#                               model='silero_vad',
+#                               force_reload=True,
+#                               onnx=USE_ONNX)
+
+# (get_speech_timestamps,
+#  save_audio,
+#  read_audio,
+#  VADIterator,
+#  collect_chunks) = utils
+
+def vad_seg(audio_file, output_file):
+    pid = multiprocessing.current_process().pid
+    (get_speech_timestamps,
+ save_audio,
+ read_audio,
+ VADIterator,
+ collect_chunks) = vad_utils[pid]
+    sr = librosa.get_samplerate(audio_file)
+    sound = read_audio(audio_file, sampling_rate=sr)
+    speech_timestamps = get_speech_timestamps(sound, vad_models[pid], sampling_rate=sr)
+    # pprint(speech_timestamps)
+    save_audio(output_file,
+           collect_chunks(speech_timestamps, sound), sampling_rate=sr) 
+
+def get_va_length(audio_file):
+    pid = multiprocessing.current_process().pid
+    (get_speech_timestamps,
+    save_audio,
+    read_audio,
+    VADIterator,
+    collect_chunks) = vad_utils[pid]
+    sr = librosa.get_samplerate(audio_file)
+    sound = read_audio(audio_file, sampling_rate=sr)
+    speech_timestamps = get_speech_timestamps(sound.to('cuda'), vad_models[pid], sampling_rate=sr)
+    va_length = sum([ts['end'] - ts['start'] for ts in speech_timestamps])
+    
+    return {"active_length": va_length / sr, "original_length": len(sound) / sr, "active_ratio": va_length / len(sound)}
+
+def identify_language(audio_file):
+    pid = multiprocessing.current_process().pid
+    # assumed a vad file
+    audio = whisper.load_audio(audio_file)
+    audio = whisper.pad_or_trim(audio)
+    
+    mel = whisper.log_mel_spectrogram(audio).to(whisper_models[pid].device)
+    _, probs = whisper_models[pid].detect_language(mel)
+    
+    sorted_dict = {k: v for k, v in sorted(probs.items(), key=lambda item: item[1], reverse=True)}
+
+    # Get the top 10 items
+    top_10 = dict(list(sorted_dict.items())[:10])
+    return top_10
+    
---
+++ post_process/youtube/verify_vad_mp.py
@@ -0,0 +1,66 @@
+from val_utils_mp import init_vad_model, get_va_length
+from glob import glob
+import os
+import random
+from tqdm import tqdm
+import multiprocessing as mp
+import json
+import fire
+
+def check_folder(root):
+    mp3_files = glob(os.path.join(root, '**', '*.mp3'), recursive=True)
+    # if len(mp3_files) < trail:
+    #     return {"ms": 0, "en": 0}
+    res = []
+    pid = os.getpid()  # Get process ID
+    for f in tqdm(mp3_files, desc=f"PID: {pid}"):  # Display PID in progress bar description
+        f_res = get_va_length(f)
+        f_res['file_name'] = f
+        res.append(f_res)
+        # print(f_res)
+    return res
+
+def verify_folder(args):
+    root, folder = args
+    init_vad_model()
+    res = check_folder(os.path.join(root, folder))
+    return res
+    
+def verify_all(root, workers=4):
+    sub_dirs = ['@Misstamchiak', '@QianyuSG', 
+           '@jusjusrunsworld', '@JeremyFPS', '@TheDailyKetchupPodcast', '@DarenYoong', 
+           '@singtel', '@swanhangoutz', '@SingaporeEye', '@JazePhua', '@mindefsg', 
+           '@DefensePoliticsAsia', '@SingaporeCivilDefenceForce', '@thecommonfolkssg9263', 
+           '@TheMichelleChongChannel', '@CathedraloftheGoodShepherdSG', '@tri333ple', '@tzechi', 
+           '@pxdkitty', '@SGRoadVigilanteSGRV', '@Jecsphotos', '@MYBBTC', '@YouGotWatch', '@tradingwithrayner', 
+           '@JTeamSingapore', '@mrbrown', '@rwsentosa', '@LADIESFIRSTTVSG', '@OGS.Official', '@WiserBiker', 
+           '@joshconsultancy', '@jianhao', '@BenjaminKhengMusic', '@TheRSAF', '@SGHeavyVehicles', '@PNNKomsan', 
+           '@3Wheelingtots', '@sgcarmartreviews', '@DoctorTristanPeh', '@ZULAsg', '@EatbookYT', '@jebbey', '@TheHabitsDoctor', 
+           '@DrWealthVideos', '@itsclarityco', '@Butterworks', '@ricemediaco', '@nikfatimahismail', '@MotoristSg', '@MothershipSG', 
+           '@ggbleed', '@HoneyMoneySG', '@ninijiang', '@easwaranspeaks3511', '@pmosingapore', '@TheIkigaiZone', '@thetitanpodcast', 
+           '@CNAInsider', '@SingaporeTourismBoard', '@corporatebreakoutcouple', '@sgheadline', '@AlexTeo', '@nopengoo', 
+           '@overkillsingapore', '@goodyfeedbluecats', '@asiaone', '@ridhwannabe', '@TemasekDigital', '@MuslimSg', 
+           '@ICA_Singapore', '@sosolomon22', '@OfficialZX', '@TiffwithMi', '@theonlinecitizenasia', '@TinyCreatureHub', 
+           '@SGRV', '@OKLETSGO', '@tambakoverlanders', '@threesixzero', '@ImasPhua', '@VisitSingapore', '@RunnerKao', 
+           '@TheFifthPersonChannel', '@ProvidendSG', '@wpsgp', '@qhventures', '@TheSmartLocal', '@cikgurebekah3957', 
+           '@OurSingaporeArmy', '@Sixiderchannel', '@BusyDaddyCooks', '@AJVMFISHING', '@thewokesalaryman', '@SUSUTVC', 
+           '@NTUCFairPriceSG', '@ProgressSingaporePartyOfficial', '@bpjeyaytc', '@SingaporePoliceForce', '@thewinsty', 
+           '@bongqiuqiu', '@DanielTamago', '@ImanFandiAhmad', '@ChurchOfTheHolyCrossSingapore', '@teamtitanshorts', '@Zeeebo', 
+           '@ShawnIskandar', '@CatholicSG', '@TheTradingGeek', '@SingaporeLifeChurch', '@evaleeqxx', '@paranormalboyz179', 
+           '@ieatishootipost', '@samsungsingapore', '@GovTechSG', '@DemiZhuang', '@Swizzyinsg', '@BasicModelsManagement', '@myancrypto', 
+           '@zaobaodotsg', '@HomeCentralsg', '@thetengcompany', '@savvyericchiew', '@canalplusmyanmarfg', '@MandaiWildlifeReserve', '@TheBackstageBunch', '@ViuSingapore', '@WELLOshow', '@joeyyap', '@SMRTCorpSG', '@Follow_ussg', '@SupernaturalConfessions', '@JustKeepThinking', '@DearStraightPeople', '@KelvinLearnsInvesting', '@KFCSingapore', '@singaporeair', '@BenRanAway', '@MissFizaO', '@FASTVSingapore', '@kingkongmp', '@straitstimesonline', '@McDSG', '@MOMsingapore', '@AimRun', '@JosephPrince', '@saltandlightsg', '@CPFvideos', '@LTAsingapore', '@TODAYonline', '@1m65', '@chenliao585', '@speedkakireviews', '@OompaLoompaCycling', '@cartoonnetworkasia', '@Sethisfy', '@ZermattNeo', '@wahbanana', '@SmallCityIsland', '@theroycelee', '@GospelPartnerOfficial', '@spudstudy', '@AnnetteLeeMusic', '@TEAMTITANOFFICIAL', '@fcbcsg', '@TYPICALSG', '@KleoYan', '@PastorJasonLim', '@endowus', '@rebelssquad', '@smashsports2003', '@CaffeMartellaSingapore']
+    params = [(root, sub_dir) for sub_dir in sub_dirs]
+    
+    over_all = []
+    with mp.Pool(workers) as p:
+        # Each result is processed separately and tqdm is used to show progress for each folder
+        results = list(tqdm(p.imap_unordered(verify_folder, params), total=len(params), desc="Main Progress"))
+    
+    for res in results:
+        over_all.extend(res)
+    
+    with open('vad_para.json', 'w') as f:
+        json.dump(over_all, f)
+    
+if __name__ == '__main__':
+    fire.Fire(verify_all)
